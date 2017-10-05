c26a502500a86594c9597309fc502af5375a1c70
Ladislas de Toldi <ladislas@leka.io>
2017-10-05 18:17:54 -0400

    refactor setCommand

--- upload.js
+++ upload.js
@@ -6,26 +6,28 @@ const {exec} = require('child_process');
 const updateButtonEl = document.getElementById('updateButton');
 
 function setCommand() {
+	let command;
+
 	if (os.type() == 'Darwin') {
 		const avrdude = './bin/avrdude';
 		const conf    = '-C ./bin/osx-avrdude.conf'
 		const flags   = '-v -p atmega2560 -D -c wiring -b 115200 -P /dev/tty.usbmodem*'
-		const flash   = '-U flash:w:' + hexFile + ':i';
+		const flash   = '-U flash:w:' + hexPath + ':i';
+		command = [avrdude, conf, flags, flash].join(" ");
 	}
 
 	if (os.type() == 'Windows_NT') {
 		const avrdude = './bin/avrdude.exe';
 		const conf    = '-C ./bin/windows-avrdude.conf'
 		const flags   = '-v -p atmega2560 -D -c wiring -b 115200 -P COM*'
-		const flash   = '-U flash:w:' + hexFile + ':i';
+		const flash   = '-U flash:w:' + hexPath + ':i';
+		command = [avrdude, conf, flags, flash].join(" ");
 	}
 
-	return [avrdude, conf, flags, flash].join(" ");
+	return command;
 }
 
 function update() {
-	console.log("Hex file path: " + hexFile);
-
 	const command = setCommand();
 
 	console.log(command);
