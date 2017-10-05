5cc4d6fc8fa4ac96ce6d31b1e411a23ca878f816
Ladislas de Toldi <ladislas@leka.io>
2017-10-05 18:20:52 -0400

    improved drop.js - throw error when dropping multiple files - throw error when dropping non hex file - refactor the code and variable names

--- drop.js
+++ drop.js
@@ -1,27 +1,61 @@
 'use strict';
 
-const holder = document.getElementById('drop');
-let hexFile;
+const dropZone = document.getElementById('dropZone');
+const dropInstruction = document.getElementById('dropInstruction');
 
-holder.ondragover = () => {
+let hexPath;
+
+function setReaction (string, color, duration = 0.5) {
+	dropZone.style = 'background: ' + color + '; transition: ' + duration + 's;';
+	dropInstruction.innerHTML = string;
+}
+
+dropZone.ondragover = () => {
+	return false;
+}
+
+dropZone.ondragenter = (e) => {
+	setReaction("Yep here! That's right! :)", "#4CAF50");
 	return false;
 };
 
-holder.ondragleave = () => {
+dropZone.ondragleave = () => {
+	setReaction("Oooh!... Come back! :(", "#EEEEEE");
+
+	setTimeout(function(){
+		setReaction("Drag and drop the <code>.hex</code> file here.", "#EEEEEE");
+	}, 3000);
+
 	return false;
 };
 
-holder.ondragend = () => {
+dropZone.ondragend = () => {
+	setReaction("Drag and drop the <code>.hex</code> file here.", "#EEEEEE")
 	return false;
 };
 
-holder.ondrop = function (e) {
+dropZone.ondrop = function (e) {
+
 	e.preventDefault();
+	dropZone.style = "background: #EEEEEE; transition: 0.5s;";
 
-	for (let f of e.dataTransfer.files) {
-		hexFile = f.path;
-		console.log('File(s) you dragged here: ', f.path);
+	if (e.dataTransfer.files.length != 1) {
+		dropInstruction.innerHTML = "Only one file should be dropped. Please try again."
+		return false;
 	}
+
+	const file          = e.dataTransfer.files[0];
+	const fileName      = file.name;
+	const filePath      = file.path;
+	const fileExtension = fileName.slice((fileName.lastIndexOf(".") - 1 >>> 0) + 2);
+
+	if (fileExtension != 'hex') {
+		dropInstruction.innerHTML = 'You dropped a <code>' + fileExtension + '</code> file.</br>The file must be a <code>.hex</code> file. Please try again.'
+		return false;
+	}
+
+	hexPath = filePath;
+	dropInstruction.innerHTML = 'Well done! We are about to upload <code>'+ fileName + '</code></br>You can now click the Upload button bellow.'
 
 	return false;
 };
