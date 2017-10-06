370fab3abaeb0cf4223a4efeddeacd4d9d508609
Ladislas de Toldi <ladislas@leka.io>
2017-10-05 23:40:42 -0400

    wip spawn

--- upload.js
+++ upload.js
@@ -1,17 +1,40 @@
 'use strict';
 
 const os = require('os');
-const {exec} = require('child_process');
+const { exec, spawn } = require('child_process');
+
+let avrdude = undefined;
+let args   = [];
+
+function setupCommand() {
+	if (os.type() == 'Darwin') {
+		const conf    = ('-C ./bin/osx-avrdude.conf').split(' ');
+		const flags   = ('-v -p atmega2560 -D -c wiring -b 115200 -P /dev/tty.usbmodem1421').split(' ');
+		const flash   = ('-U flash:w:' + hexPath + ':i').split(' ');
+
+		avrdude = './bin/avrdude';
+		args = args.concat(conf, flags, flash);
+	}
+
+	if (os.type() == 'Windows_NT') {
+		const conf    = ('-C ./bin/windows-avrdude.conf').split(' ');
+		const flags   = ('-v -p atmega2560 -D -c wiring -b 115200 -P COM*').split(' ');
+		const flash   = ('-U flash:w:' + hexPath + ':i').split(' ');
+
+		avrdude = './bin/avrdude.exe';
+		args = args.concat(conf, flags, flash);
+	}
+}
+
 
 function setCommand() {
-	let command;
-
 	if (os.type() == 'Darwin') {
 		const avrdude = './bin/avrdude';
 		const conf    = '-C ./bin/osx-avrdude.conf'
 		const flags   = '-v -p atmega2560 -D -c wiring -b 115200 -P /dev/tty.usbmodem*'
 		const flash   = '-U flash:w:' + hexPath + ':i';
-		command = [avrdude, conf, flags, flash].join(" ");
+
+		return [avrdude, conf, flags, flash].join(" ");
 	}
 
 	if (os.type() == 'Windows_NT') {
@@ -19,16 +42,13 @@ function setCommand() {
 		const conf    = '-C ./bin/windows-avrdude.conf'
 		const flags   = '-v -p atmega2560 -D -c wiring -b 115200 -P COM*'
 		const flash   = '-U flash:w:' + hexPath + ':i';
-		command = [avrdude, conf, flags, flash].join(" ");
-	}
 
-	return command;
+		return [avrdude, conf, flags, flash].join(" ");
+	}
 }
 
 function update() {
 	const command = setCommand();
-
-	console.log(command);
 
 	exec(command, (err, stdout, stderr) => {
 		if (err) {
@@ -44,4 +64,33 @@ function update() {
 	});
 }
 
-updateButtonEl.addEventListener('click', update);
+function spawnUpdate() {
+	setupCommand();
+	const cmd = spawn(avrdude, args);
+
+	cmd.on('error', function( err ){ throw err })
+
+	cmd.stdout.on('data', (data) => {
+		console.log(`stdout: ${data}`);
+	});
+
+	cmd.stderr.on('data', (data) => {
+		console.log(`stderr: ${data}`);
+		if (data.includes('timeout')) {
+			cmd.kill();
+			console.log('process killed');
+		}
+		if (data.includes('mismatch')) {
+			cmd.kill();
+			spawnUpdate();
+		}
+	});
+
+	cmd.on('close', (code) => {
+		console.log(`child process exited with code ${code}`);
+		cmd.kill();
+		console.log('process killed');
+	});
+}
+
+updateButtonEl.addEventListener('click', spawnUpdate);
