f8e99dd215e27f10fbdbc0340666f2a55acc6b63
Ladislas de Toldi <ladislas@leka.io>
2017-10-09 12:24:57 +0200

    fix issue with path to execute avrdude command

--- upload.js
+++ upload.js
@@ -1,22 +1,23 @@
 'use strict';
 
 const os = require('os');
+const path = require('path');
 const {exec} = require('child_process');
 
 function setCommand() {
 	let command;
 
 	if (os.type() == 'Darwin') {
-		const avrdude = './bin/avrdude';
-		const conf    = '-C ./bin/osx-avrdude.conf'
-		const flags   = '-v -p atmega2560 -D -c wiring -b 115200 -P /dev/tty.usbmodem*'
+		const avrdude = path.join(__dirname, 'bin/avrdude');
+		const conf    = '-C' + ' ' + path.join(__dirname, './bin/osx-avrdude.conf');
+		const flags   = '-v -p atmega2560 -D -c wiring -b 115200 -P /dev/tty.usbmodem*';
 		const flash   = '-U flash:w:' + hexPath + ':i';
 		command = [avrdude, conf, flags, flash].join(" ");
 	}
 
 	if (os.type() == 'Windows_NT') {
-		const avrdude = './bin/avrdude.exe';
-		const conf    = '-C ./bin/windows-avrdude.conf'
+		const avrdude = path.join(__dirname, 'bin/avrdude.exe');
+		const conf    = '-C' + path.join(__dirname, './bin/windows-avrdude.conf');
 		const flags   = '-v -p atmega2560 -D -c wiring -b 115200 -P COM*'
 		const flash   = '-U flash:w:' + hexPath + ':i';
 		command = [avrdude, conf, flags, flash].join(" ");
